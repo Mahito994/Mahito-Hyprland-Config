f91a463f7047898863901037277f25c244209423
Mahito994 <24mahito994@gmail.com>
2026-01-10 13:00:34 +0100

    first commit

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      python = { "black" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      vue = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+    },
+  },
+}
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,8 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    signature_help = {
+      enabled = false,
+    },
+  },
+}
