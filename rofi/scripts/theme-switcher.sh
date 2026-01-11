83a1c4b51928b753ac887b0865ce78b24c01120a
Mahito994 <24mahito994@gmail.com>
2026-01-11 22:09:33 +0100

    added a theme switcher for rofi

---
+++ rofi/scripts/theme-switcher.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ---------------- CONFIG ----------------
+KITTY_CONF="$HOME/.config/kitty/kitty.conf"
+KITTY_THEME_DIR="$HOME/.config/kitty/kitty-themes"
+
+WAYBAR_CSS="$HOME/.config/waybar/style.css"
+WAYBAR_COLOR_DIR="$HOME/.config/waybar/colors/"
+
+ROFI_CMD="rofi -dmenu -i -p Theme"
+# ----------------------------------------
+
+# Get available themes from kitty theme directory
+themes=$(ls "$KITTY_THEME_DIR" | sed 's/\.conf$//')
+
+# Rofi selection
+selected=$(echo "$themes" | $ROFI_CMD)
+
+# Exit if nothing selected
+[ -z "$selected" ] && exit 0
+
+# ---------------- APPLY KITTY ----------------
+sed -i \
+  "s|^include .*kitty-themes/.*\.conf|include kitty-themes/$selected.conf|" \
+  "$KITTY_CONF"
+
+# Reload kitty
+pkill -USR1 kitty
+
+# ---------------- APPLY WAYBAR ---------------
+ln -sf "$WAYBAR_COLOR_DIR/$selected.css" "$WAYBAR_COLOR_DIR/current.css"
+
+./.config/waybar/scripts/launch.sh
+
+# ---------------- SENDS NOTIFICATION ---------
+notify-send "Theme changed" "$selected applied to Kitty & Waybar"
